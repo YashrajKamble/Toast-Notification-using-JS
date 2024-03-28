7771ab5258d6ba76b3ed3330e78611ed2479e2d3
Yashraj_K <yashrajkamble7727@gamil.com>
2024-03-28 16:19:35 +0530

    add notification bar

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let toastBox = document.getElementById('toastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"> </i> Successfully Submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"> </i> Please Fix the Error !';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"> </i> Invalid Input, Check Again';
 
-function showToast() {
+
+function showToast(msg) {
     let toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerHTML = 'success';
+    toast.innerHTML = msg;
     toastBox.appendChild(toast);
+
+    if (msg.includes('Error')) {
+        toast.classList.add('error');
+    }
+    if (msg.includes('Invalid')) {
+        toast.classList.add('invalid');
+    }
 }
